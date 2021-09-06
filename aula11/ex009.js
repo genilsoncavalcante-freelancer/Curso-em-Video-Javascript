018cca1ace2138d1bff4adbc2c927acae2dbe1e5
Genilson Cavalcante de Oliveira <genilsoncavalcante.freelancer@gmail.com>
2021-09-06 17:10:46 -0300

    Delete aula11 directory

--- aula11/ex009.js
+++
@@ -1,9 +0,0 @@
-var pais = `Brasil`
-
-pais = pais.toUpperCase()//Passando para letras maiúsculas
-
-if (pais == `BRASIL`) {
-    console.log('Brasileiro!')
-}else {
-    console.log(`Estrangeiro`)
-}
